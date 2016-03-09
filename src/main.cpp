311965dcaf7875e8f5268098e35ae85b3bd3d540
fuho <ondrej.dolejsi@gmail.com>
2016-03-08 22:01:22 -0500

    A bunch of new functions to draw to display.

--- src/main.cpp
+++ src/main.cpp
@@ -9,73 +9,19 @@ const char* ssid = "N16FU";
 const char* password = "masdostfest";
 const uint8_t clockPin = D5;
 const uint8_t dataPin = D7;
-// Set the number of LEDs to control.
-const uint16_t ledCount = 144;
-// Create a buffer for holding the colors (3 bytes per color).
+
+const uint8_t displayWidth = 18;
+const uint8_t displayHeight = 8;
+const uint16_t ledCount = displayWidth * displayHeight;
+rgb_color brushColor = (rgb_color){0,0,0}; // Default brush color is black
 rgb_color colors[ledCount];
-// Set the brightness to use (the maximum is 31).
 const uint8_t brightness = 1;
-// We define "power" in this sketch to be the product of the
-// 8-bit color channel value and the 5-bit brightness register.
-// The maximum possible power is 255 * 31 (7905).
-const uint16_t maxPower = 255 * 31;
-
-// The power we want to use on the first LED is 1, which
-// corresponds to the dimmest possible white.
-const uint16_t minPower = 1;
-
-// Calculate what the ratio between the powers of consecutive
-// LEDs needs to be in order to reach the max power on the last
-// LED of the strip.
-const float multiplier = pow(maxPower / minPower, 1.0 / (ledCount - 1));
 
 ESP8266WebServer server(80);
 APA102<dataPin, clockPin> ledStrip;
+int counter = 0; // Server access count
 
-int counter = 0;
 
-
-// This function sends a white color with the specified power,
-// which should be between 0 and 7905.
-void sendWhite(uint16_t power)
-{
-  // Choose the lowest possible 5-bit brightness that will work.
-  uint8_t brightness5Bit = 1;
-  while(brightness5Bit * 255 < power && brightness5Bit < 31)
-  {
-    brightness5Bit++;
-  }
-
-  // Uncomment this line to simulate an LED strip that does not
-  // have the extra 5-bit brightness register.  You will notice
-  // that roughly the first third of the LED strip turns off
-  // because the brightness8Bit equals zero.
-  //brightness = 31;
-
-  // Set brightness8Bit to be power divided by brightness5Bit,
-  // rounded to the nearest whole number.
-  uint8_t brightness8Bit = (power + (brightness5Bit / 2)) / brightness5Bit;
-
-  // Send the white color to the LED strip.  At this point,
-  // brightness8Bit multiplied by brightness5Bit should be
-  // approximately equal to power.
-  ledStrip.sendColor(brightness8Bit, brightness8Bit, brightness8Bit, brightness5Bit);
-}
-
-void handleNotFound(){
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-}
 
 // Converts a color from HSV to RGB.
 // h is hue, as a number between 0 and 360.
@@ -99,16 +45,64 @@ rgb_color hsvToRgb(uint16_t h, uint8_t s, uint8_t v)
     return (rgb_color){r, g, b};
 }
 
+void clearDisplay(){
+    for(int i=0;i<ledCount;i++){
+        colors[i]={0,0,0};
+    }
+}
 
+void randomizeDisplay(){
+    for(int i=0;i<ledCount;i++){
+        colors[i]={rand() % 255, rand() % 255, rand() % 255};
+    }
+}
+
+void setBrushColor(rgb_color color){
+ brushColor = color;
+}
+
+void setBrushColor(uint8_t r,uint8_t g,uint8_t b){
+ brushColor = {r,g,b};
+}
+
+void drawPixel(uint8_t x, uint8_t y){
+    if(x >= displayWidth || y >= displayHeight) return; // Drawing outside disp.
+    colors[y * displayWidth + x] = brushColor;
+}
+
+void updateDisplay(){
+      ledStrip.write(colors, ledCount, brightness);
+}
+
+void drawRect(uint8_t x, uint8_t y, uint8_t width, uint8_t height){
+    uint8_t maxX = x + width;
+    uint8_t maxY = y + height;
+    for(int y = 0; y <= maxY; y++){
+        if(y == 0 || y == maxY ){ // only fill if top or bottom or req
+            for(int x = 0; x <= maxX; x++){
+                drawPixel(x,y);
+            }
+        } else {
+            drawPixel(x,y);
+            drawPixel(maxX,y);
+        }
+    }
+}
+
+void drawSolidRect(uint8_t x, uint8_t y, uint8_t width, uint8_t height){
+    uint8_t maxX = x + width;
+    uint8_t maxY = y + height;
+    for(int y = 0; y <= maxY; y++){
+        for(int x = 0; x <= maxX; x++){
+            drawPixel(x,y);
+        }
+    }
+}
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Booting");
-
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
     delay(5000);
     ESP.restart();
   }
@@ -119,66 +113,24 @@ void setup() {
   ArduinoOTA.setHostname("esp8266fu");
   // No authentication by default
   // ArduinoOTA.setPassword((const char *)"123");
-  ArduinoOTA.onStart([]() {
-    Serial.println("Start");
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
   ArduinoOTA.begin();
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
   
   server.on("/", [](){
     server.send(200, "text/plain", "Accessed " + String(++counter) + " times.");
   });
-  server.onNotFound(handleNotFound);
   server.begin();
-  Serial.println("HTTP server started");
 }
+
 
 void loop() {
   ArduinoOTA.handle();
   server.handleClient();
-  
-  uint8_t time = millis() >> 2;
-    for(uint16_t i = 0; i < ledCount; i++)
-    {
-      byte x = time - i;
-      colors[i] = hsvToRgb((uint32_t)x * 359 / 256, 255, 255);
-    }
-  ledStrip.write(colors, ledCount, brightness);
-/*
-  uint8_t time = millis() >> 2;
-  for(uint16_t i = 0; i < ledCount; i++)
-  {
-    uint8_t x = time - i * 8;
-    colors[i].red = x;
-    colors[i].green = 255 - x;
-    colors[i].blue = 255-x;
-  }
-  ledStrip.write(colors, ledCount, brightness);
-*/
-/*
-  ledStrip.startFrame();
-  float power = minPower;
-  for(uint16_t i = 0; i < ledCount; i++)
-  {
-    sendWhite(power);
-    power = power * multiplier;
-  }
-  ledStrip.endFrame(ledCount);
-*/
+  clearDisplay();
+  setBrushColor(255,255,255);
+  drawRect(1,1,16,6);
+  //setBrushColor(187,255,19);
+  //drawSolidRect(0,5,7,10);
+  //randomizeDisplay();
+  updateDisplay();
+  delay(30);
 }
